2afd89859f48de238a9867e95cfe32309e158ef0
1035610621@qq.com <1035610621@qq.com>
2018-12-13 22:18:40 +0800

    todolist

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,26 @@
+<template>
+ <li @click="handleClick" class="item">{{content}}</li>
+</template>
+
+<script>
+export default {
+ props:['content','index'],
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+   handleClick(index){
+      this.$emit('delete',this.index)
+   }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style >
+.item{
+  color:green;
+}
+</style>
