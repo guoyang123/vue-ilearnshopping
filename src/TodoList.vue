1a9db48e639f7cd237de2567de959b4b8239c8f4
neuedu-win <neuedu-win@qq.com>
2018-12-13 17:35:07 +0800

    todo

---
+++ src/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+ <div>
+   <input v-model="inputvalue"/>
+   <button @click="handleClick">提交</button>
+   <ul>
+     <li>1</li>
+     <li>2</li>
+   </ul>
+ </div>
+</template>
+
+<script>
+export default {
+ data:function(){
+   return {
+     inputvalue:"",
+     list:[]
+   }
+ },
+  methods:{
+    handleClick(){
+     this.list.push(this.inputvalue)
+      this.inputvalue=""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
